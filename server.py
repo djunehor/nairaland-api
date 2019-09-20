5db21916f4db86dcdbf3ff9815a463fe28325d84
makinde2013 <adeademi2@gmail.com>
2019-09-20 11:56:26 +0100

    fix variable error

--- server.py
+++ server.py
@@ -38,7 +38,7 @@ if os.environ.get('Heroku') == 'True':
     heroku = True
 
     chrome_options = Options()
-    options.headless = True
+    chrome_options.headless = True
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument("--start-maximized")
     chrome_options.add_argument("--disable-infobars")
