96eb3db4139e2046cd85fe03a2509e8631f33900
makinde2013 <adeademi2@gmail.com>
2019-10-08 16:45:24 +0100

    add circleCI

--- app/app.py
+++ app/app.py
@@ -518,4 +518,4 @@ def user_post_share():
 # Start Flask
 ####################################################################
 if __name__ == '__main__':
-    app.run(debug=True, port = int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, host='0.0.0.0', port = int(os.environ.get("PORT", 5000)))
