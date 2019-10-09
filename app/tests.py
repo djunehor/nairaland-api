59cb8914d2cc0a8a20442990d4c116c764bc0c65
makinde2013 <adeademi2@gmail.com>
2019-10-09 01:19:28 +0100

    add dependencies

--- app/tests.py
+++ app/tests.py
@@ -89,9 +89,5 @@ class TestCase(unittest.TestCase):
         response = self.app.get("/user/shared_with")
         self.assertEqual(response.status_code, 200)
 
-    def test_fetch_user_posts_shared_with(self):
-        response = self.app.get("/user/shared_with")
-        self.assertEqual(response.status_code, 200)
-
 if __name__ == "__main__":
     unittest.main()
